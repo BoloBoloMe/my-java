276f9b5ba3f80ac6d660594ee602a67e99ff5e83
luojingyan <luojingyan@lizhi.fm>
2022-05-27 16:52:28 +0800

    提交 aqs 相关的代码

--- src/test/java/com/example/workspace/juc/AqsUseCase.java
+++ src/test/java/com/example/workspace/juc/AqsUseCase.java
@@ -5,13 +5,12 @@ package com.example.workspace.juc;
  * Date:2022/5/23 23:17
  */
 public class AqsUseCase {
+
     public static void main(String[] args) {
         ReentrantLock lock = new ReentrantLock();
-        lock.newCondition();
         lock.lock();
         try {
             // do something
-
         } finally {
             lock.unlock();
         }
